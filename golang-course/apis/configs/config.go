34fc3e4af3e1e771a17caa6f806cfc136cdfd680
Fabricio Bonjorno <bonjorno@mac.home>
2025-11-30 15:35:26 -0500

    Set mapstructs

--- golang-course/apis/configs/config.go
+++ golang-course/apis/configs/config.go
@@ -9,15 +9,15 @@ var cfg *conf
 
 // conf holds the application configuration
 type conf struct {
-	DBDriver      string
-	DBHost        string
-	DBPort        string
-	DBUser        string
-	DBPassword    string
-	DBName        string
-	WebServerPort string
-	JWTSecret     string
-	JWTExpiresIn  int
+	DBDriver      string           `mapstructure:"DB_DRIVER"`
+	DBHost        string           `mapstructure:"DB_HOST"`
+	DBPort        string           `mapstructure:"DB_PORT"`
+	DBUser        string           `mapstructure:"DB_USER"`
+	DBPassword    string           `mapstructure:"DB_PASSWORD"`
+	DBName        string           `mapstructure:"DB_NAME"`
+	WebServerPort string           `mapstructure:"WEB_SERVER_PORT"`
+	JWTSecret     string           `mapstructure:"JWT_SECRET"`
+	JWTExpiresIn  int              `mapstructure:"JWT_EXPIRESIN"`
 	TokenAuth     *jwtauth.JWTAuth // JWT authentication instance
 }
 
