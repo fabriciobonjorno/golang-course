318c9b98c4164ab17003037ecb1c3b3422516033
Fabricio Bonjorno <bonjorno@mac.home>
2025-12-05 14:25:21 -0500

    Config routes with chi

--- golang-course/apis/cmd/server/main.go
+++ golang-course/apis/cmd/server/main.go
@@ -1,5 +1,48 @@
 package main
 
+import (
+	"apis/configs"
+	"apis/internal/entity"
+	"apis/internal/infra/database"
+	"apis/internal/infra/webserver/handlers"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
 func main() {
-	// Entry point for the server application
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&entity.User{}, &entity.Product{})
+
+	// Initialize Product database and handler here
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB) // Create ProductHandler
+
+	// using Chi router or any other router of your choice
+	r := chi.NewRouter()
+
+	// logs all requests
+	r.Use(middleware.Logger)
+
+	// Register the CreateProduct route
+	r.Post("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":8000", r)
+
+	// Register the CreateProduct route
+	// using default mux for simplicity
+	// http.HandleFunc("/products", productHandler.CreateProduct)
+
+	// http.ListenAndServe(":8000", nil)
 }
